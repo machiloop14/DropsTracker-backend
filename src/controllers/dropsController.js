eb203a7c670863b4d475455b98a38605fcd1c071
machiloop <hamidusodiq2@gmail.com>
2026-04-03 02:47:27 +0100

    feat: implement endpoint for fetching user airdrops

--- src/controllers/dropsController.js
+++ src/controllers/dropsController.js
@@ -1,15 +1,22 @@
+import { success } from "zod";
 import { prisma } from "../db.js";
 
 export const handleAddAirdrop = async (req, res) => {
   let savedAirdrop;
+
+  //fetch zod-validated data
   const airdropDetails = req.validatedData;
 
+  //fetch current user(from isAuth middleware using jwt)
   const userId = req.user.id;
+
+  //if no current user, return error
   if (!userId)
     return res
       .status(400)
       .json({ success: false, message: "No user in current request" });
 
+  //save the airdrop in database
   try {
     savedAirdrop = await prisma.airdrop.create({
       data: { ...airdropDetails, userId },
@@ -18,9 +25,39 @@ export const handleAddAirdrop = async (req, res) => {
     return res.status(400).json({ success: false, message: error });
   }
 
+  //return a success response
   res.status(200).json({
     success: true,
     message: "drops route reached",
     data: savedAirdrop,
   });
+};
+
+export const handleFetchAirdrops = async (req, res) => {
+  //fetch current user(from isAuth middleware using jwt)
+  try {
+    const userId = req.user.id;
+
+    //if no current user, return error
+    if (!userId)
+      return res
+        .status(400)
+        .json({ success: false, message: "No user in current request" });
+
+    //fetch all drops of current user
+    const fetchedAirdrops = await prisma.airdrop.findMany({
+      where: { userId: userId },
+    });
+
+    return res.status(200).json({
+      success: true,
+      message: "All airdrops fetched successfully",
+      data: fetchedAirdrops,
+    });
+  } catch (error) {
+    return res.status(400).json({
+      success: false,
+      message: error.message ? error.message : "Error. Try again later",
+    });
+  }
 };
--- src/routes/dropsRoute.js
+++ src/routes/dropsRoute.js
@@ -2,11 +2,15 @@ import express from "express";
 import isAuth from "../middlewares/isAuth.js";
 import { dropSchema } from "../validators/dropSchema.js";
 import validateSchema from "../middlewares/validate.js";
-import { handleAddAirdrop } from "../controllers/dropsController.js";
+import {
+  handleAddAirdrop,
+  handleFetchAirdrops,
+} from "../controllers/dropsController.js";
 
 const router = express.Router();
 
 router.use(isAuth);
 router.post("/", validateSchema(dropSchema), handleAddAirdrop);
+router.get("/", handleFetchAirdrops);
 
 export default router;
